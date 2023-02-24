fe3e97f48aa7816d12639277ceab6a6c44fe1f09
surfinzap <surfinzap@users.noreply.github.com>
2023-02-24 21:09:56 +0100

    🔨 Add watcher for file changes

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const { spawn } = require('child_process');
+
+
+
+let watchPaths = [
+  'templates/b-default-vscode.mustache',
+  'schemes/b-default.json'
+]
+
+
+let generator = 'theme-generator.py'
+
+gulp.task('watch', function() {
+  watch(watchPaths, function() {
+    console.log('Regenarating theme...');
+    const python = spawn('python', [generator]);
+
+    // listens for data events emitted by the standard output stream (stdout) of the child process spawned by the spawn function.
+    python.stdout.on('data', function(data) {
+      console.log(data.toString());
+    });
+    python.stderr.on('data', function(data) {
+      console.error(data.toString());
+    });
+  });
+});
