9a11ac1d6f48444dcdf658f2d03c51e70a3667d7
surfinzap <surfinzap@users.noreply.github.com>
2023-03-22 17:04:15 +0100

    🔨 Remove files before new batch

--- theme-generator.py
+++ theme-generator.py
@@ -6,11 +6,27 @@ import os
 colors = json.loads(open('colors/colors.json', 'r').read())
 schemes_dir = 'schemes/'
 template = open('templates/b-default-vscode.mustache', 'r').read()
-theme_prefix = 'themes/b-theme-'
+theme_folder = 'themes/'
+theme_prefix = 'b-theme-'
 
 
+def delete_files(folder_path): 
+  files = os.listdir(folder_path)
 
-def process_schemes(folder_path, process_func):
+  # iterate over the files and delete them
+  for file_name in files:
+    file_path = os.path.join(folder_path, file_name)
+    try:
+      if os.path.isfile(file_path):
+        os.remove(file_path)
+    except Exception as e:
+      print(f"Failed to delete {file_path}. Reason: {e}")
+
+
+def process_schemes(folder_path, render_theme):
+  # cleaning up folder
+  delete_files(theme_folder)
+
   schemes_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
 
   themes_list = [] # that’s for populating for package.json
@@ -24,10 +40,10 @@ def process_schemes(folder_path, process_func):
       themes_list.append({
         "label" : f"{scheme_json['name']}",
         "uiTheme" : f"vs-{scheme_json['type']}",
-        "path" : f"./{theme_prefix + scheme_file}"
+        "path" : f"./{theme_folder + theme_prefix + scheme_file}"
       })
 
-      process_func(scheme_json, scheme_file)
+      render_theme(scheme_json, scheme_file)
 
   # updating package.json
   with open('package.json', 'r') as f:
@@ -42,7 +58,7 @@ def process_schemes(folder_path, process_func):
 
 def render_theme(scheme, filename):
   partial_render = pystache.render(template, scheme) 
-  theme_filename = theme_prefix + filename
+  theme_filename = theme_folder + theme_prefix + filename
   with open(theme_filename, 'w') as f:
     f.write(pystache.render(partial_render, colors))
     print('Theme generated: ' + theme_filename)
